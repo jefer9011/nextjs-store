c1562893a3bb963c3f7fbca19ec31432b1202ad0
Jeff Code <jeferson9011@gmail.com>
2025-01-26 18:51:32 -0500

    Refactor MainProducts component to fetch products from Shopify API and update Hero component styles with accent color

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,8 +1,27 @@
+const getProducts = async () => {
 
-export const MainProducts = () => {
-    return (
-        <div>
-        <h3>Main Products</h3>
-        </div>
-    );
-}
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2025-01/products.json`,
+    {
+      headers: {
+        'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || "",
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return data;
+};
+
+export const MainProducts = async () => {
+    const products = await getProducts();
+
+    console.log('products', products);
+    
+  return (
+    <div>
+      <h3>Main Products</h3>
+    </div>
+  );
+};

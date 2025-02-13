d4c8758a40b8880b4a0a268aaf66b41f45e75bf1
Jeff Code <jeferson9011@gmail.com>
2025-02-13 11:28:38 -0500

    Add client-side redirection for product view: implement redirect to home if product is not found

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,7 @@
 import { ProductView } from "app/components/product/ProductView/ProductView"
 import { getProducts } from "app/services/shopify/products"
+import { redirect } from "next/navigation"
+
 
 interface ProductPageProps {
   searchParams: {
@@ -11,6 +13,10 @@ export default async function ProductPage({ searchParams }: ProductPageProps) {
   const id = searchParams.id;
   const products = await getProducts(id);
   const product = products[0];
+/* 
+  if(!id) {
+    redirect('/store');
+  } */
 
   return <ProductView product={product} />
 }
